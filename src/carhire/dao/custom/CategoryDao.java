a0a31b4c6d4443b0982cf277c2cb7416243f9f2b
UdaraDhanushka <udaradanush@gmail.com>
2023-09-25 23:05:26 +0530

    Dao file adding

---
+++ src/carhire/dao/custom/CategoryDao.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package carhire.dao.custom;
+
+import carhire.dao.CrudDao;
+
+/**
+ *
+ * @author UDARA DHANUSHKA
+ */
+public interface CategoryDao {
+    public interface OrderDao extends CrudDao<CategoryEntity, String>{
+}
